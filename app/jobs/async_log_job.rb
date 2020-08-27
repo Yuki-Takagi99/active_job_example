d56a10d0ba59f1100b284456ac17542e46c3f233
yuki takagi <yuki99takagi@gmail.com>
2020-08-27 11:40:34 +0900

    Add: 5-1-5 複数キューの管理

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -1,5 +1,12 @@
 class AsyncLogJob < ApplicationJob
-  queue_as :default
+  queue_as do
+    case self.arguments.first[:message]
+    when "to async_log"
+      :async_log
+    else
+      :default
+    end
+  end
 
   def perform(message: "hello") # message引数を追加
     AsyncLog.create!(message: message) # DBに保存する
