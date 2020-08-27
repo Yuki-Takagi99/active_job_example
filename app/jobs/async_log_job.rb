23eb6dcb3eaf99e67d741993537ddb8f18b0d378
yuki takagi <yuki99takagi@gmail.com>
2020-08-27 11:46:11 +0900

    Add: 5-1-6 ジョブの例外処理

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -8,6 +8,14 @@ class AsyncLogJob < ApplicationJob
     end
   end
 
+  retry_on StandardError, wait: 5.seconds, attempts: 3
+  # 複数の例外クラスを指定することも可能
+  # retry_on ArgumentError, ZeroDivisionError, wait: 5.seconds, attempts: 3
+
+  discard_on StandardError do |job, error|
+    SomeNotifier.push(error)
+  end
+
   def perform(message: "hello") # message引数を追加
     AsyncLog.create!(message: message) # DBに保存する
   end
